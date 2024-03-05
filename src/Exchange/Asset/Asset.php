cb643ac10689af5db96d2bc79af8a8497dbf8415
Mateus Guimarães <mateus.jatene@gmail.com>
2024-03-04 22:39:48 -0300

    fix tests

--- src/Exchange/Asset/Asset.php
+++ src/Exchange/Asset/Asset.php
@@ -5,8 +5,8 @@ namespace Modules\Exchange\Asset;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Modules\Exchange\Money;
 use Modules\Exchange\MoneyCast;
-use Money\Money;
 
 class Asset extends Model
 {
